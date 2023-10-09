a50c5febb49960f3eadeb978c4c49d7c92b6652c
AlbertV1810 <alvargas@hartford.edu>
2023-10-09 19:22:17 -0400

    made diamondInput

--- Diamond.java
+++ Diamond.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
     int a = 0;
+    int diamondInput = input.next();
     Scanner input = new Scanner(System.in);
-
   }
 }
