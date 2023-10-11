8369ca1ebd4ab92376884a031a9ee18381175463
AlbertV1810 <alvargas@hartford.edu>
2023-10-11 14:18:05 -0400

    added int a

--- Diamond.java
+++ Diamond.java
@@ -3,9 +3,10 @@ public class Diamond {
   public static void main(String[] args) {
     System.out.println("Enter input: ");
     Scanner input = new Scanner(System.in);
+    int a = input.nextInt();
+    
 
 
-
-
+    
   }
 }
