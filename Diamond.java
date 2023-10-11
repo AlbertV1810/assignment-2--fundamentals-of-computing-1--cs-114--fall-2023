b24166cb186bc438a7caa7ebde7f6441081bc23b
AlbertV1810 <alvargas@hartford.edu>
2023-10-11 14:32:55 -0400

    replaced a with diamondInput

--- Diamond.java
+++ Diamond.java
@@ -3,9 +3,13 @@ public class Diamond {
   public static void main(String[] args) {
     System.out.println("Enter input: ");
     Scanner input = new Scanner(System.in);
+    int diamondInput = input.nextInt();
 
-    for(int a = input.nextInt(); a > 0; a--);
+    for( ; diamondInput > 0; diamondInput--);{
+      System.out.println("Your input now is: " + numberDiamond);
+      System.out.print();
 
+    }
 
     input.close();
   }
