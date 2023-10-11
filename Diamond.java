9de55124f8b88fa780c478bcc023396854d33289
AlbertV1810 <alvargas@hartford.edu>
2023-10-11 14:16:24 -0400

    removed for loop attempt and diamondInput

--- Diamond.java
+++ Diamond.java
@@ -3,9 +3,9 @@ public class Diamond {
   public static void main(String[] args) {
     System.out.println("Enter input: ");
     Scanner input = new Scanner(System.in);
-    int diamondInput = input.nextInt();
 
-    for(int a = 1; a < diamondInput; a++);
+
+
 
   }
 }
