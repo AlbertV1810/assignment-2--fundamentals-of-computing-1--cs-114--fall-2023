2cf4ef6685523384a1f0ae95707e4d3d3399864d
AlbertV1810 <alvargas@hartford.edu>
2023-10-15 16:44:46 -0400

    added for loop to print stars

--- Diamond.java
+++ Diamond.java
@@ -5,14 +5,20 @@ public class Diamond {
     Scanner input = new Scanner(System.in);
     int diamondInput = input.nextInt();
 
-    if (diamondInput%2 == 1){
+    if (diamondInput%2 == 1)
+    {
       for(int spaceInput = 0;spaceInput<diamondInput;spaceInput++)
       {
-        for(int charInput = 0;charInput<diamondInput-1;charInput++)
+        for(int charInput = 0;charInput<diamondInput-spaceInput;charInput++)
         {
-            System.out.print(" ");
+          System.out.print(" ");
+        }
+        for(int charInput=0;charInput<=spaceInput;charInput++)
+        {
+          System.out.print("*");
         }
       }
+      System.out.println();
 
     }
 
