fbb35a287ec6442a293536ec3a24c9a7b9dba0cc
AlbertV1810 <alvargas@hartford.edu>
2023-10-15 19:03:05 -0400

    changed spaceChars to 1 and -1 2forloop

--- Diamond.java
+++ Diamond.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Diamond {
   public static void main(String[] args) {
-    System.out.print("Enter input: ");
+    System.out.println("Enter input: ");
     Scanner input = new Scanner(System.in);
     int diamondInput = input.nextInt();
 
